7fc4c4e782876f3cb711006405517ac0fe770237
Abdulrahman Alasmari <ahalasmari@uqu.edu.sa>
2017-05-23 01:59:27 +0300

    Added browser sync and sass tasks

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+
+gulp.task('browser-sync', () => {
+    browserSync({
+        proxy: "localhost:5555",
+        port: 4444,
+        notify: true
+    });
+});
+
+
+gulp.task('sass', () => {
+    return gulp.src('./assets/scss/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./assets/scss/**/*.scss', ['sass', reload]);
+    gulp.watch('./views/**/*.handlebars', reload);
+});
+
+
+gulp.task('default', ['browser-sync','sass', 'watch']);
