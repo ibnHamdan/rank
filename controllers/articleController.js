cab20d696e7fca09975238d13a53483ebfa0241e
Abdulrahman Alasmari <ahalasmari@uqu.edu.sa>
2017-07-09 07:17:37 +0300

    Added upload and resize images controllers

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,4 +1,19 @@
 const db = require('../db');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+        const isPhoto = file.mimetype.startsWith('image/');
+        if(isPhoto){
+        next(null, true)
+        } else {
+            next({message: ' that filetype isn\t allowed:'}, false)
+        }
+    }
+}
 
 
 exports.index = (req, res) => {
@@ -9,8 +24,25 @@ exports.add = (req, res) => {
     res.render('add');
 }
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    if( !req.file){
+        next();
+        return;
+    }
+    const extention = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extention}`;
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    next();
+
+}
+
+
 exports.addArticle =   (req, res) => {
-    db.connection.query('insert into articles set ?', req.body, function(err, result) {
+    db.connection.query('insert into article set ?', req.body, function(err, result) {
         if (err) {
             console.error(err);
             return;
@@ -21,7 +53,7 @@ exports.addArticle =   (req, res) => {
 }
 
 exports.getArticle = (req, res) => {
-    db.connection.query('select * from articles', function(err, result) {
+    db.connection.query('select * from article', function(err, result) {
          res.render('index', {result});
     });
    
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const authorController = require('../controllers/authorController');
 
 router.get('/',  articleController.getArticle, articleController.index);
 router.get('/add', articleController.add);
-router.post('/add', articleController.addArticle);
+router.post('/add', articleController.upload, articleController.resize, articleController.addArticle);
 
 router.get('/registe', authorController.registe);
 router.post('/registe', authorController.register);
