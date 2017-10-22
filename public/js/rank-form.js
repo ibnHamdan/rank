cbfeada5e87ecf12d8ffc4dea59d31245063a750
Abdulrahman Alasmari <ahalasmari@uqu.edu.sa>
2017-10-22 21:23:57 +0300

    Added user photo form

---
+++ public/js/rank-form.js
@@ -0,0 +1,9 @@
+// Array.from(document.querySelectorAll('.rank-form .field input')).forEach(i => {
+//   i.addEventListener('invalid', () => {
+//     i.dataset.touched = true
+//   })
+//   i.addEventListener('blur', () => {
+//     if (i.value !== '') i.dataset.touched = true
+//   })
+// })
+"use strict";
